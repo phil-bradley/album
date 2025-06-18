67cf417e04e7ea59d7f4c461333af3e78f6afbd3
phil-bradley <philb@mobius.ie>
2025-06-18 17:31:12 +0100

    Added tests

--- src/test/java/ie/philb/album/model/PageEntryModelTest.java
+++ src/test/java/ie/philb/album/model/PageEntryModelTest.java
@@ -4,6 +4,7 @@
  */
 package ie.philb.album.model;
 
+import ie.philb.album.util.ImageUtils;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
@@ -124,5 +125,215 @@ public class PageEntryModelTest {
 
         pem.resetOffset();
         assertEquals(new Point(0, 0), pem.getImageViewOffset());
+    }
+
+    @Test
+    void givenPageEntryModel_andImageSet_andZoomIsOne_expectViewImageNotExceedsCellBound() {
+        PageCell cell = new PageCell(new Dimension(1, 1), new Point(0, 0));
+        PageEntryModel pem = new PageEntryModel(cell);
+        pem.setImageFile(TEST_IMAGE_FILE);
+        pem.setZoomFactor(1);
+
+        PageGeometry geometry = PageGeometry.square(1);
+        PageModel pageModel = new PageModel(geometry, PageSize.A4_Landscape).withMargin(0);
+
+        Dimension viewSize = PageSize.A4_Landscape.size(); // 1-1 size mapping
+        PageGeometryMapper geometryMapper = new PageGeometryMapper(pageModel, viewSize);
+
+        BufferedImage viewImage = pem.getViewImage(viewSize, geometryMapper);
+
+        Dimension imageSize = ImageUtils.getImageSize(viewImage);
+        assertEquals(viewSize.width, imageSize.width);
+
+        int imageHeight = ImageUtils.getHeightFromWidth(viewImage.getWidth(), ImageUtils.getAspectRatio(imageSize));
+        assertEquals(imageHeight, imageSize.height);
+    }
+
+    @Test
+    void givenPageEntryModel_andImageSet_andZoomIsTen_expectViewImageNotExceedsCellBound() {
+        PageCell cell = new PageCell(new Dimension(1, 1), new Point(0, 0));
+        PageEntryModel pem = new PageEntryModel(cell);
+        pem.setImageFile(TEST_IMAGE_FILE);
+        pem.setZoomFactor(10);
+
+        PageGeometry geometry = PageGeometry.square(1);
+        PageModel pageModel = new PageModel(geometry, PageSize.A4_Landscape).withMargin(0);
+
+        Dimension viewSize = PageSize.A4_Landscape.size(); // 1-1 size mapping
+        PageGeometryMapper geometryMapper = new PageGeometryMapper(pageModel, viewSize);
+
+        BufferedImage viewImage = pem.getViewImage(viewSize, geometryMapper);
+
+        Dimension imageSize = ImageUtils.getImageSize(viewImage);
+        assertEquals(viewSize.width, imageSize.width);
+
+        int imageHeight = ImageUtils.getHeightFromWidth(viewImage.getWidth(), ImageUtils.getAspectRatio(imageSize));
+        assertEquals(imageHeight, imageSize.height);
+    }
+
+    @Test
+    void givenPageEntryModel_andImageSet_andOffsetPositive_expectViewReduced() {
+        PageCell cell = new PageCell(new Dimension(1, 1), new Point(0, 0));
+        PageEntryModel pem = new PageEntryModel(cell);
+        pem.setImageFile(TEST_IMAGE_FILE);
+        pem.setZoomFactor(1);
+
+        Point offset = new Point(100, 0);
+        pem.setImageViewOffset(offset);
+
+        PageGeometry geometry = PageGeometry.square(1);
+        PageModel pageModel = new PageModel(geometry, PageSize.A4_Landscape).withMargin(0);
+
+        Dimension viewSize = PageSize.A4_Landscape.size(); // 1-1 size mapping
+        PageGeometryMapper geometryMapper = new PageGeometryMapper(pageModel, viewSize);
+
+        BufferedImage viewImage = pem.getViewImage(viewSize, geometryMapper);
+
+        Dimension imageSize = ImageUtils.getImageSize(viewImage);
+        assertEquals(viewSize.width - offset.x, imageSize.width);
+    }
+
+    @Test
+    void givenPageEntryModel_andImageSet_andOffsetNegative_expectViewReduced() {
+        PageCell cell = new PageCell(new Dimension(1, 1), new Point(0, 0));
+        PageEntryModel pem = new PageEntryModel(cell);
+        pem.setImageFile(TEST_IMAGE_FILE);
+        pem.setZoomFactor(1);
+
+        Point offset = new Point(-100, 0);
+        pem.setImageViewOffset(offset);
+
+        PageGeometry geometry = PageGeometry.square(1);
+        PageModel pageModel = new PageModel(geometry, PageSize.A4_Landscape).withMargin(0);
+
+        Dimension viewSize = PageSize.A4_Landscape.size(); // 1-1 size mapping
+        PageGeometryMapper geometryMapper = new PageGeometryMapper(pageModel, viewSize);
+
+        BufferedImage viewImage = pem.getViewImage(viewSize, geometryMapper);
+
+        Dimension imageSize = ImageUtils.getImageSize(viewImage);
+        assertEquals(viewSize.width - Math.abs(offset.x), imageSize.width);
+    }
+
+    @Test
+    void givenPageEntryModel_andImageSet_andZoomIsOne_expectScaledImageNotExceedsCellBound() {
+        PageCell cell = new PageCell(new Dimension(1, 1), new Point(0, 0));
+        PageEntryModel pem = new PageEntryModel(cell);
+        pem.setImageFile(TEST_IMAGE_FILE);
+        pem.setZoomFactor(1);
+
+        PageGeometry geometry = PageGeometry.square(1);
+        PageModel pageModel = new PageModel(geometry, PageSize.A4_Landscape).withMargin(0);
+
+        Dimension viewSize = PageSize.A4_Landscape.size(); // 1-1 size mapping
+        PageGeometryMapper geometryMapper = new PageGeometryMapper(pageModel, viewSize);
+
+        BufferedImage scaled = pem.getScaledImage(viewSize, geometryMapper);
+
+        Dimension imageSize = ImageUtils.getImageSize(scaled);
+        assertEquals(viewSize.width, imageSize.width);
+
+        int imageHeight = ImageUtils.getHeightFromWidth(scaled.getWidth(), ImageUtils.getAspectRatio(imageSize));
+        assertEquals(imageHeight, imageSize.height);
+    }
+
+    @Test
+    void givenPageEntryModel_andImageSet_andZoomIsTen_expectScaledImageExceedsCellBound() {
+        PageCell cell = new PageCell(new Dimension(1, 1), new Point(0, 0));
+        PageEntryModel pem = new PageEntryModel(cell);
+        pem.setImageFile(TEST_IMAGE_FILE);
+        pem.setZoomFactor(10);
+
+        PageGeometry geometry = PageGeometry.square(1);
+        PageModel pageModel = new PageModel(geometry, PageSize.A4_Landscape).withMargin(0);
+
+        Dimension viewSize = PageSize.A4_Landscape.size(); // 1-1 size mapping
+        PageGeometryMapper geometryMapper = new PageGeometryMapper(pageModel, viewSize);
+
+        BufferedImage scaled = pem.getScaledImage(viewSize, geometryMapper);
+
+        Dimension imageSize = ImageUtils.getImageSize(scaled);
+        assertEquals(viewSize.width * 10, imageSize.width);
+
+        int imageHeight = ImageUtils.getHeightFromWidth(scaled.getWidth(), ImageUtils.getAspectRatio(imageSize));
+        assertEquals(imageHeight, imageSize.height);
+    }
+
+    @Test
+    void givenPageEntryModel_andImageSet_andZoomToCover_expectScaledImagesExceedsCellBound() {
+        PageCell cell = new PageCell(new Dimension(1, 1), new Point(0, 0));
+        PageEntryModel pem = new PageEntryModel(cell);
+        pem.setImageFile(TEST_IMAGE_FILE);
+        pem.setZoomFactor(1);
+
+        PageGeometry geometry = PageGeometry.square(1);
+        PageModel pageModel = new PageModel(geometry, PageSize.A4_Landscape).withMargin(0);
+
+        Dimension viewSize = PageSize.A4_Landscape.size(); // 1-1 size mapping
+        PageGeometryMapper geometryMapper = new PageGeometryMapper(pageModel, viewSize);
+        pem.zoomToCoverFit(viewSize);
+
+        BufferedImage scaled = pem.getScaledImage(viewSize, geometryMapper);
+
+        Dimension imageSize = ImageUtils.getImageSize(scaled);
+        assertEquals(viewSize.height, imageSize.height);
+
+        assertTrue(imageSize.width > viewSize.width);
+    }
+
+    @Test
+    void givenPageEntryModel_andImageSet_andZoomToCover_expectViewImagesMatchesCellBound() {
+        PageCell cell = new PageCell(new Dimension(1, 1), new Point(0, 0));
+        PageEntryModel pem = new PageEntryModel(cell);
+        pem.setImageFile(TEST_IMAGE_FILE);
+        pem.setZoomFactor(1);
+
+        PageGeometry geometry = PageGeometry.square(1);
+        PageModel pageModel = new PageModel(geometry, PageSize.A4_Landscape).withMargin(0);
+
+        Dimension viewSize = PageSize.A4_Landscape.size(); // 1-1 size mapping
+        PageGeometryMapper geometryMapper = new PageGeometryMapper(pageModel, viewSize);
+        pem.zoomToCoverFit(viewSize);
+
+        BufferedImage viewImage = pem.getViewImage(viewSize, geometryMapper);
+
+        Dimension imageSize = ImageUtils.getImageSize(viewImage);
+        assertEquals(viewSize, imageSize);
+    }
+
+    @Test
+    void givenPageEntryModel_andImageNotSet_expectPlaceholderImage() {
+        PageCell cell = new PageCell(new Dimension(1, 1), new Point(0, 0));
+        PageEntryModel pem = new PageEntryModel(cell);
+
+        PageGeometry geometry = PageGeometry.square(1);
+        PageModel pageModel = new PageModel(geometry, PageSize.A4_Landscape).withMargin(0);
+
+        Dimension viewSize = PageSize.A4_Landscape.size(); // 1-1 size mapping
+        PageGeometryMapper geometryMapper = new PageGeometryMapper(pageModel, viewSize);
+
+        BufferedImage viewImage = pem.getViewImage(viewSize, geometryMapper);
+        Dimension viewImageSize = ImageUtils.getImageSize(viewImage);
+
+        Dimension placeHolderImageSize = ImageUtils.getImageSize(ImageUtils.getPlaceholderImage());
+        assertEquals(placeHolderImageSize, viewImageSize);
+    }
+
+    @Test
+    void givenPageEntryModel_andImageNotSet_andCellIsSmall_expectPlaceholderImage() {
+        PageCell cell = new PageCell(new Dimension(1, 1), new Point(0, 0));
+        PageEntryModel pem = new PageEntryModel(cell);
+
+        PageGeometry geometry = PageGeometry.square(1);
+        PageModel pageModel = new PageModel(geometry, PageSize.A4_Landscape).withMargin(0);
+
+        Dimension viewSize = new Dimension(5, 5); // Too small for placeholder
+        PageGeometryMapper geometryMapper = new PageGeometryMapper(pageModel, viewSize);
+
+        BufferedImage viewImage = pem.getViewImage(viewSize, geometryMapper);
+        Dimension viewImageSize = ImageUtils.getImageSize(viewImage);
+
+        Dimension placeHolderImageSize = new Dimension(0, 0);
+        assertEquals(placeHolderImageSize, viewImageSize);
     }
 }
