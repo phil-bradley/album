99f892d1e01117a33820cb32918b84af438d2909
philb <philb@ithil>
2025-06-20 23:09:33 +0100

    Added metadata tests

---
+++ src/test/java/ie/philb/album/metadata/ImageMetaDataTest.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ie.philb.album.metadata;
+
+import java.awt.Dimension;
+import java.io.File;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author philb
+ */
+public class ImageMetaDataTest {
+
+    @Test
+    void validateReadImageMetaData() throws Exception {
+
+        String resourcePath = getClass().getResource("/test_275x183.jpg").getPath();
+        File file = new File(resourcePath);
+
+        ImageMetaDataReader imageMetaDataReader = new ImageMetaDataReader(file);
+        ImageMetaData imageMetaData = imageMetaDataReader.getMetaData();
+
+        assertNotNull(imageMetaData);
+
+        assertEquals(275, imageMetaData.getWidth());
+        assertEquals(183, imageMetaData.getHeight());
+        assertEquals(new Dimension(275, 183), imageMetaData.getSize());
+
+        assertEquals(300, imageMetaData.getXResolution());
+        assertEquals(300, imageMetaData.getYResolution());
+        assertEquals(new Dimension(300, 300), imageMetaData.getResolution());
+
+    }
+}
