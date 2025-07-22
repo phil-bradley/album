393ac5251c7dfce2b75f332a6fc328741d36bb34
philb <philb@ithil>
2025-07-22 21:02:08 +0100

    Add Jlayer to AlbumOverviewPanel to intercept mouse events

--- src/main/java/ie/philb/album/view/AlbumOverviewPanel.java
+++ src/main/java/ie/philb/album/view/AlbumOverviewPanel.java
@@ -7,14 +7,15 @@ package ie.philb.album.view;
 import ie.philb.album.AppContext;
 import ie.philb.album.ui.common.AppPanel;
 import ie.philb.album.ui.common.GridBagCellConstraints;
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.AWTEvent;
+import java.awt.Component;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JComponent;
 import javax.swing.JLayer;
 import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
 import javax.swing.plaf.LayerUI;
 
 /**
@@ -34,42 +35,36 @@ public class AlbumOverviewPanel extends AppPanel {
 
         // Simulate a glasspane in order to capture mouse events, that is,
         // prevent them from being handled by lower level elements.
-        JLayer<AlbumView> layeredAlbumView = new JLayer<>(albumView, new LayerUI<>());
-
-//            @Override
-//            public void paint(Graphics g, JComponent c) {
-//                super.paint(g, c);
-//
-            ////                Graphics2D g2 = (Graphics2D) g;
-////                g2.setColor(new Color(0, 0, 0, 100));  // translucent black
-////                g2.fillRect(0, 0, getWidth(), getHeight());
-//            }
-
-//            @Override
-//            protected void processMouseEvent(MouseEvent e, JLayer<? extends AlbumView> l) {
-//                e.consume();  //  block event from reaching underlying components
-//                System.out.println("Mouse event " + e);
-//            }
-//        });
-
-//        layeredAlbumView.setVisible(true);
-//        layeredAlbumView.setOpaque(false);
-//        setVisible(true);
-
-        AppPanel testPanel = filler(new Dimension(1000, 100), Color.CYAN);
-        albumView.addMouseListener(new MouseAdapter() {
+        LayerUI<JScrollPane> mouseInterceptLayer = new LayerUI<>() {
             @Override
-            public void mouseClicked(MouseEvent e) {
-                System.out.println("Mouse clicked " + e);
+            public void installUI(JComponent c) {
+                super.installUI(c);
+                ((JLayer<?>) c).setLayerEventMask(AWTEvent.MOUSE_EVENT_MASK);
             }
-        });
+
+            @Override
+            public void uninstallUI(JComponent c) {
+                ((JLayer<?>) c).setLayerEventMask(0);
+                super.uninstallUI(c);
+            }
+
+            @Override
+            protected void processMouseEvent(MouseEvent e, JLayer<? extends JScrollPane> l) {
+                if (e.getID() == MouseEvent.MOUSE_CLICKED) {
+                    Component target = SwingUtilities.getDeepestComponentAt(l.getView(), e.getX(), e.getY());
+                    System.out.println("Intercepted click at: " + e.getPoint() + ", would have hit " + target);
+                }
+            }
+        };
 
         this.scrollPane = new JScrollPane();
         this.scrollPane.setViewportView(albumView);
         this.scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
         this.scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 
-        add(scrollPane, new GridBagCellConstraints().fillBoth().weight(1));
+        JLayer<JScrollPane> layeredAlbumView = new JLayer<>(scrollPane, mouseInterceptLayer);
+
+        add(layeredAlbumView, new GridBagCellConstraints().fillBoth().weight(1));
         addComponentListener(new ResizeListener());
 
         setFocusable(true);
