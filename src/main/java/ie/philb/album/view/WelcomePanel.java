6e4326367949302917e892cfcd40542839b02828
philb <philb@ithil>
2026-04-14 20:11:15 +0100

    Update WelcomePanel

--- src/main/java/ie/philb/album/view/WelcomePanel.java
+++ src/main/java/ie/philb/album/view/WelcomePanel.java
@@ -7,7 +7,12 @@ package ie.philb.album.view;
 import ie.philb.album.ui.common.AppPanel;
 import ie.philb.album.ui.common.GridBagCellConstraints;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 /**
  *
@@ -15,15 +20,25 @@ import javax.swing.JLabel;
  */
 public class WelcomePanel extends AppPanel {
 
+    private static final float WELCOME_FONT_SIZE = 36f;
+    private static final Color WELCOME_FONT_COLOR = new Color(10, 202, 245);
+    
     private JLabel lblMain;
 
     public WelcomePanel() {
-        background(Color.cyan);
+        background(Color.white);
+        initComponents();
     }
 
     private void initComponents() {
+        
         lblMain = new JLabel();
+        lblMain.setHorizontalAlignment(SwingConstants.CENTER);
+        lblMain.setFont(getFont().deriveFont(WELCOME_FONT_SIZE));
+        lblMain.setForeground(WELCOME_FONT_COLOR);
         lblMain.setText("Welcome!!");
+        
+        layoutComponents();
     }
 
     private void layoutComponents() {
@@ -31,8 +46,25 @@ public class WelcomePanel extends AppPanel {
         GridBagCellConstraints gbc = new GridBagCellConstraints()
                 .anchorNorth()
                 .fillHorizontal()
-                .weight(1);
+                .weight(1)
+                .insetTop(30);
 
         add(lblMain, gbc);
+    }
+
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame();
+        frame.getContentPane().setBackground(Color.yellow);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setLayout(new GridBagLayout());
+        frame.setPreferredSize(new Dimension(400, 250));
+        frame.setSize(frame.getPreferredSize());
+        
+        GridBagCellConstraints gbc = new GridBagCellConstraints().fillBoth().weight(1);
+        
+        WelcomePanel welcomePanel = new WelcomePanel();
+        frame.add(welcomePanel, gbc);
+        frame.setVisible(true);
     }
 }
